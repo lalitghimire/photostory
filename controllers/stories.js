af0c72533a100ce8d2f4efb4c1e0d5de32a351c5
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-01 20:21:31 +0300

    add authentication create stories route

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,8 +1,26 @@
 import Story from '../models/Story.js';
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+const getToken = (req) => {
+    const auth = req.get('authorization');
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        return auth.substring(7);
+    }
+    return null;
+};
 
 const addStory = async (req, res) => {
+    const token = getToken(req);
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedToken.id) {
+        return res.json({ error: 'token missing or invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
+
     const story = req.body;
-    const newStory = new Story(story);
+
+    const newStory = new Story({ ...story, user: user.username });
     try {
         await newStory.save();
         res.send(newStory);
