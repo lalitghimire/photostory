a8d35274d0eacff760027653e878477c80ebff1e
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-01 20:15:57 +0300

    add user register/login functions

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+const register = async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPw = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPw,
+        });
+
+        const user = await newUser.save();
+        res.send(user);
+    } catch (error) {
+        res.send(error);
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.send('User not found');
+
+        const validate = await bcrypt.compare(req.body.password, user.password);
+        if (!validate) return res.send('Password incorrect');
+
+        const jwttoken = jwt.sign({ email: user.email, id: user._id }, secret, {
+            expiresIn: '1h',
+        });
+        res.send({ user: { name: user.username, email: user.email, id: user._id }, jwttoken });
+    } catch (error) {
+        res.send(error);
+    }
+};
+
+export { register, login };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,7 @@
 import express from 'express';
 const router = express.Router();
+import { register, login } from '../controllers/users.js';
 
+router.post('/register', register);
+router.post('/login', login);
 export default router;
