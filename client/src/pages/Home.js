c3d3dd7a1da1d20505c3c1ecb2549f4c56cfaaa3
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-04 22:53:02 +0300

    add grid container to add Story components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Story from '../components/Story.js';
+import { Grid } from '@mui/material';
+
+const Home = () => {
+    return (
+        <Grid container margin='2px' padding='2px' alignItems='auto' spacing={3}>
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} lg={2.4}>
+                <Story></Story>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Home;
