178e8f1f76ba8149eb7b61bc2938d30f063f7a97
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-04 22:51:13 +0300

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import { Container } from '@mui/material';
 
 const App = () => {
-    return <div>Hello new storybook</div>;
+    return (
+        <Container maxWidth='xl'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </Container>
+    );
 };
 
 export default App;
