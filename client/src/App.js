ab1fe565d78c6a5fba2802c1d1935f7a09200405
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-05 16:39:48 +0300

    add login & register routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import { Container } from '@mui/material';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
     return (
@@ -10,6 +12,8 @@ const App = () => {
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
             </Routes>
         </Container>
     );
