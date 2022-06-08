371ce45a98419189948927a4c460e46584af9b79
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-09 00:02:18 +0300

    add react toastify

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,14 @@ import Header from './components/Header';
 import { Container } from '@mui/material';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
     return (
         <Container maxWidth='xl'>
             <Header />
+            <ToastContainer position='top-center' />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
