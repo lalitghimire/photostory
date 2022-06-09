7fff8df5784c053ca3349c9c47f5656b0fb2a2ee
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-09 22:41:20 +0300

    fetch and store backend data

--- client/src/redux/features/storySlice.js
+++ client/src/redux/features/storySlice.js
@@ -1,9 +1,138 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const url = `http://localhost:5000`;
+
+export const addStory = createAsyncThunk(
+    'stories/addStory',
+    async (newStory, { rejectWithValue }) => {
+        try {
+            const response = await axios.post(`${url}/stories`, newStory);
+            toast.success('A new story has been added');
+            return response.data;
+        } catch (error) {
+            toast.error(' New story could not be added');
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const getAllStories = createAsyncThunk(
+    'stories/getStories',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await axios.get(`${url}/stories`);
+            return response.data;
+        } catch (error) {
+            toast.error('Unable to fetch stories');
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const removeStory = createAsyncThunk(
+    'stories/removeStory',
+    async (id, { rejectWithValue }) => {
+        try {
+            const response = await axios.delete(`${url}/stories/${id}`);
+            toast.warn('A story has been deleted');
+            return response.data;
+        } catch (error) {
+            toast.error(`Couldn't delete the story`);
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const updateStory = createAsyncThunk(
+    'stories/updateStory',
+    async (toBeUpdatedStory, { rejectWithValue }) => {
+        try {
+            const response = await axios.put(
+                `${url}/stories/${toBeUpdatedStory._id}`,
+                toBeUpdatedStory
+            );
+            toast.success(`Story has been updated`);
+            return response.data;
+        } catch (error) {
+            toast.error(`Couldn't update Story`);
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+const initialState = {
+    stories: [],
+    isLoading: false,
+
+    error: null,
+};
 
 const storySlice = createSlice({
-    name: 'stories',
-    initialState: {},
+    name: 'story',
+    initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addStory.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(addStory.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.stories.push(action.payload);
+                state.error = null;
+            })
+            .addCase(addStory.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+
+            .addCase(getAllStories.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getAllStories.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.stories = action.payload;
+                state.filteredContacts = action.payload;
+                state.error = null;
+            })
+            .addCase(getAllStories.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+
+            .addCase(removeStory.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(removeStory.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.stories = state.stories.filter((c) => c._id !== action.payload.id);
+                state.error = null;
+            })
+            .addCase(removeStory.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+
+            .addCase(updateStory.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(updateStory.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.stories = state.stories.map((story) =>
+                    story._id === action.payload._id ? action.payload : story
+                );
+                state.error = null;
+            })
+            .addCase(updateStory.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
+
+//export const {  } = contactsSlice.actions; //use when there is something in reducer
 
 export default storySlice.reducer;
