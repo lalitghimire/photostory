68044ad26dd7c9b9de8cc9ecd622862e92dfdd4c
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-09 22:36:36 +0300

    add items to appbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,47 @@
-import { AppBar } from '@mui/material';
+import React from 'react';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setLogout } from '../redux/features/authSlice';
 
 const Header = () => {
+    const { user } = useSelector((state) => ({ ...state.authReducer }));
+    const dispatch = useDispatch();
+
     return (
-        <header style={{ textAlign: 'center' }}>
-            <AppBar position='static' style={{ background: '#199ca8', borderRadius: '20px' }}>
-                {' '}
-                <h1>Photo story book </h1>{' '}
-            </AppBar>
-        </header>
+        <AppBar
+            position='static'
+            style={{ align: 'center', background: '#7db1e8', borderRadius: '20px' }}
+        >
+            {' '}
+            <Toolbar>
+                <Typography variant='h4' display='flex' flexGrow={1}>
+                    Photo story book
+                </Typography>{' '}
+                {user?.user?.email && <Typography>signed in as {`${user.user.email}`}</Typography>}
+                <Link to={'/'}> Home </Link>
+                {user ? (
+                    <Button
+                        onClick={() => dispatch(setLogout())}
+                        color='secondary'
+                        variant='contained'
+                        style={{ margin: 10 }}
+                    >
+                        Sign Out
+                    </Button>
+                ) : (
+                    <Button
+                        component={Link}
+                        to='/login'
+                        color='secondary'
+                        variant='contained'
+                        style={{ margin: 10 }}
+                    >
+                        Sign In
+                    </Button>
+                )}
+            </Toolbar>
+        </AppBar>
     );
 };
 
