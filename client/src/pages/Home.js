0be8de5af5f20d19b411ccb51c76438b323f8e63
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-09 22:39:09 +0300

    render stories from backend to page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,57 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Story from '../components/Story.js';
 import { Grid } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllStories } from '../redux/features/storySlice';
 
 const Home = () => {
+    const { stories, isLoading } = useSelector((state) => state.storyReducer);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllStories());
+    }, [dispatch]);
+
+    if (isLoading) {
+        return <p> Stories loading......</p>;
+    }
+    console.log('stories', stories);
     return (
-        <Grid container margin='2px' padding='2px' alignItems='auto' spacing={3}>
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={6} lg={2.4}>
-                <Story></Story>
-            </Grid>
+        <Grid container margin='0px' padding='25px' alignItems='auto' spacing={3}>
+            {stories &&
+                stories.map((story) => (
+                    <Grid item xs={12} sm={12} md={6} lg={3} key={story._id}>
+                        {' '}
+                        <Story title={story.title} user={story.user}></Story>{' '}
+                    </Grid>
+                ))}
         </Grid>
     );
 };
