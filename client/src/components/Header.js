d3264cee94d7332cf9f6567e3eff33f53c34e64c
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-13 23:18:06 +0300

    update items in appbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ import { setLogout } from '../redux/features/authSlice';
 const Header = () => {
     const { user } = useSelector((state) => ({ ...state.authReducer }));
     const dispatch = useDispatch();
-
+    console.log('user', user);
     return (
         <AppBar
             position='static'
@@ -16,10 +16,25 @@ const Header = () => {
             {' '}
             <Toolbar>
                 <Typography variant='h4' display='flex' flexGrow={1}>
-                    Photo story book
+                    <Link style={{ textDecoration: 'none', color: 'white' }} to={'/'}>
+                        {' '}
+                        Photo story book{' '}
+                    </Link>
                 </Typography>{' '}
-                {user?.user?.email && <Typography>signed in as {`${user.user.email}`}</Typography>}
-                <Link to={'/'}> Home </Link>
+                {user?.user?.email && (
+                    <Typography variant='h6'>signed in as {`${user.user.email}`}</Typography>
+                )}
+                {user && (
+                    <Button
+                        component={Link}
+                        to='/addstory'
+                        color='secondary'
+                        variant='contained'
+                        style={{ margin: 10 }}
+                    >
+                        Add Story
+                    </Button>
+                )}
                 {user ? (
                     <Button
                         onClick={() => dispatch(setLogout())}
